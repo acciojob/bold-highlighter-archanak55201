a2977fb351ecaead62f3864470214b98ca7188f8
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-27 12:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let bold= document.getElementsByTagName("strong");
+
 function highlight() {
-    //Write your code here
+  for(let i=0;i<bold.length;i++){
+    bold[i].style.color="green";}
+	//Write your code here
 
 
 }
 
 
 function return_normal() {
+  for(let i=0;i<bold.length;i++){
     //Write your code here
-
+ bold[i].style.color="black";}
     
 }
